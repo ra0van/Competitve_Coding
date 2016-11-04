dece421787a8b28342972cc9dd03f99f49cd8c43
rajasekhar vanjarapu <rajasekhar745@gmail.com>
2016-11-04 15:51:51 +0530

    added append-and-delete.py

---
+++ HackerRank/Algorithms/append-and-delete.py
@@ -0,0 +1,37 @@
+#!/bin/python
+
+#https://www.hackerrank.com/contests/w25/challenges/append-and-delete
+#10/12 passed 
+
+import sys
+
+
+def getCommonStart(a,b):
+    if len(a) > len(b):
+        a,b = b,a
+    common = ''
+    for i in range(len(a)):
+        if a[i] == b[i]:
+            common += a[i]
+        else:
+            break
+    
+    return common
+
+
+s = raw_input().strip()
+t = raw_input().strip()
+k = int(raw_input().strip())
+
+
+if s==t:
+    print "Yes"
+elif k >= len(s) + len(t):
+    print "Yes"
+else:
+    common = getCommonStart(s,t)
+    if len(common)>0 and (len(s)+len(t) -  2*len(common) == k):
+        print "Yes"
+    else:
+        print "No"
+
