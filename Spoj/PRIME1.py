e4cc3026e1a3cc8e61571638f1be944dbf30433d
rajasekhar.vanjarapu <rajasekhar745@gmail.com>
2016-11-16 17:36:11 +0530

    segmented sieve incomplete

--- Spoj/PRIME1.py
+++ Spoj/PRIME1.py
@@ -1,33 +1,37 @@
-#TLE
+# your code goes here
 import math
-def SieveOfEratosthenes(upperBound,prev=0):
-	# print "Se"
-	li = []
-	upperBoundSqrRoot = int(math.sqrt(upperBound))
-	isComposite = [False]*(upperBound+1)
-
-	for i in xrange(2,upperBoundSqrRoot+1):
-		if not isComposite[i]:
-			li.append(i)
-			for k in xrange(i*i,upperBound+1,i):
-				isComposite[k] = True
-
-	for i in xrange(upperBoundSqrRoot,upperBound+1):
-		if not isComposite[i]:
-			if i not in li:
-				li.append(i)
-	return li
-
 t = input()
-prev = -1
-li = []
-for i in range(t):
-	a,n = map(int,raw_input().split())
-	if n>prev:
-		del li[:]
-		li = SieveOfEratosthenes(n)
-		prev  = n
-	for i in li:
-		if i>=a and i<=n:
-			print i
 
+def SimpleSieve(size):
+	a = [0]*(size+1)
+	for i in range(2,size+1):
+		if a[i] == 0 :
+			j = 2
+			while j*i<=size:
+				a[j*i] = 1
+				j+=1
+	base = []
+	for i in range(2,size+1):
+		if a[i] == 0:
+			base.append(i)
+	return baseO
+
+for i in range(t):
+	x,y = map(int,raw_input().split())
+
+	limit = math.floor(math.sqrt(1000000000))
+	base = SimpleSieve(limit)
+
+	low = limit
+	high = 2*limit
+
+	prev = []
+	while low<=y:
+		a = [0] *(high-low+1)
+
+		for p in base:
+
+
+
+
+	
