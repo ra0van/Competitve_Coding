6ab344006332e0c3b8a0149673b4192a0b0aff90
rajasekhar vanjarapu <rajasekhar745@gmail.com>
2017-06-05 13:07:10 +0530

    Update XENRANK.py

--- CodeChef/17JUNE/XENRANK.py
+++ CodeChef/17JUNE/XENRANK.py
@@ -3,17 +3,6 @@ for _ in xrange(t):
   x,y = map(int,raw_input().split())
   v = x+y
   rank = v*(v+1)/2
-  xi = 0
-  yi = v
-  ranks = []
-  r = 1
-  while xi <= v and yi >= 0 :
-    if xi<=x and yi<=y:
-      ranks.append(r)
-    xi += 1 
-    yi -= 1
-    r += 1
-  print rank + max(ranks)
-  
+  print rank + (v+1 - y)
   
   
