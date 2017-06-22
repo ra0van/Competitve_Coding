4ae0ddafa4c524fea1199a713f7dc5cc0135a016
rajsherror404 <rajasekhar745@gmail.com>
2017-06-22 08:48:02 +0530

    Partial - one TLE case

--- CodeChef/ZCOPRAC/ZCO15002.py
+++ CodeChef/ZCOPRAC/ZCO15002.py
@@ -3,11 +3,24 @@ def main():
 	a = map(int,raw_input().split())
 	a.sort()
 	count = 0
-	for i in xrange(n):
-		for j in xrange(i+1,n):
-			if a[j]-a[i] >=k:
-				count += n-1-j
-				break
+	m = max(a)
+	prev = -1
+	prev_count = 0
+  	for i in xrange(n):
+	    if a[i]==prev:
+	    	count += prev_count
+	    	continue
+	    if m-a[i] < k :
+	    	prev_count = 0
+	    	prev = a[i]
+	    	continue
+	   	prev_count = 0
+	    for j in xrange(i+1,n):
+	      if a[j]-a[i] >=k:
+	        count += n-j
+	        prev_count = n-j
+	        break
+	   	prev = a[i]
 	print count
 
 if __name__ == '__main__':
