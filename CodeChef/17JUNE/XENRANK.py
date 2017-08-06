05535b557430be819514ca18f2bdbb7368e9ab5d
rajasekhar Vanjarapu <rajsherror404@gmail.com>
2017-08-06 06:20:03 +0000

    duplicate clean up

--- CodeChef/17JUNE/XENRANK.py
+++
@@ -1,8 +0,0 @@
-t = input()
-for _ in xrange(t):
-  x,y = map(int,raw_input().split())
-  v = x+y
-  rank = v*(v+1)/2
-  print rank + (v+1 - y)
-  
-  
--- CodeChef/CIELAB.py
+++
@@ -1,9 +0,0 @@
-# https://www.codechef.com/problems/CIELAB
-a,b=map(int,raw_input().split())
-c = a-b
-last = c %10
-if last <9 :
-	c += 1
-else:
-	c -= 1
-print c
